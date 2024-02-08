ea0b274a15b6c526f298d7d22cc0ac5f1d6a6d35
FaridDadashov <ferid.dadashov@icloud.com>
2024-02-08 16:08:20 +0400

    projects start

---
+++ main.js
@@ -0,0 +1,138 @@
+// task1 
+// let myArray=["cup","plate","spoon","knife","fork",]
+// let counter=0 ;
+// while(counter<myArray.length){
+//     console.log(myArray[counter]);
+//     counter++       
+// }
+
+
+// task2
+// let array=["xiyar","pomidor","sogan","yerkoku","sarimsaq","semeni"]
+// let element=0
+
+// while(element<array.length){
+//     if(array[element][0]=="s"){
+//         console.log(array[element]);
+//     }
+//     element++
+// }
+
+
+// task3
+// let cem=0
+// let reqem=1
+
+// while(reqem<=100){
+//    cem=cem+reqem
+//    reqem++ 
+   
+// }
+// console.log(`1-den 100e qeder ededlerin cemi:${cem}` );
+
+
+
+
+
+// task4
+// let massiv=["kitab","defter","qelem","karandas","jurnal","pozan","vereq"]
+// let gosterici=0
+// while(gosterici<massiv.length){
+// if(massiv[gosterici].length<=5){
+//     console.log(massiv[gosterici][4]);
+// }
+// gosterici++
+// }
+
+
+
+
+
+
+
+
+// task5
+
+// let reqem=100000
+
+// while(reqem>0){
+//     if(reqem%9999==0){
+//         console.log(`9999-a bolunen ilk eded:${reqem}`);
+//         break
+//     }
+//     reqem--
+// }
+
+
+
+// task6
+
+// let cumle="men her gun javascript oyrenirem"
+// let saitler=/[aieou]/
+// let duzelencumle=cumle.replace(saitler,' ');
+// console.log(duzelencumle);
+
+
+
+
+// FOR FOOP
+// task1
+
+// let myArray=["cup","plate","spoon","knife","fork",]
+// for(i=0;i<myArray.length;i++){
+//     console.log(myArray[i]);
+// }
+
+
+
+
+// task2
+// let array=["xiyar","pomidor","sogan","yerkoku","sarimsaq","semeni"]
+// for(i=0;i<array.length;i++){
+//     if(array[i][0]=="s"){
+//         console.log(array[i]);
+//     }
+// }
+
+
+// task3
+// let cem=0
+// for(i=0;i<=100;i++){
+//     cem=cem+i
+    
+    
+// }
+// console.log(`1-den 100-e qeder ededlerin cemi: ${cem}`);
+
+
+
+
+
+// task4
+// let massiv=["kitab","defter","qelem","karandas","jurnal","pozan","vereq"]
+// for ( i = 0; i < massiv.length; i++) {
+//     if (massiv[i].length < 5) {
+//         console.log(massiv[i]);
+//     }
+// }
+
+
+// task5
+// let a=9999
+// for(i=100000;i>0;i--){
+//    if(i%a==0){
+//     console.log(i);
+//     break
+//    }
+// }
+
+
+
+// task6
+let letter="men her gun javascript oyrenirem"
+for(i=0 ;i<letter.length ;i++){
+    if(i%2===0)
+    console.log(letter[i]);
+}
+
+
